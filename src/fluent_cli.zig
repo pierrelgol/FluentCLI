6c5acf845a6a0918cf7fb24a03ec34e26dbf6600
PierreLgol <plgol.perso@gmail.com>
2024-05-31 10:53:34 +0200

    laid down the basic structure

--- src/fluent_cli.zig
+++ src/fluent_cli.zig
@@ -1,15 +1,31 @@
 const std = @import("std");
 const testing = std.testing;
 
-pub fn FluentCLI() type {
+pub fn FluentCLI(name: []const u8) type {
     return struct {
-        const Self = @This();
-        program_name: []const u8,
+        const program_name: []const u8 = name;
 
-        pub fn init(program_name: []const u8) Self {
-            return Self{
-                .program_name = program_name,
-            };
-        }
+        pub const FluentArgument = struct {
+            arg_name: []const u8,
+            arg_desc: []const u8,
+            is_required: bool,
+            default_value: ?[]const u8,
+            validator: ?fn (arg: []const u8) bool,
+        };
+
+        pub const FluentCommand = struct {
+            flag_name: []const u8,
+            flag_shrt: []const u8,
+            flag_long: []const u8,
+            flag_desc: []const u8,
+            flag_args: []FluentArgument,
+            is_optional: bool,
+            callback: fn (@This(), anytype) bool,
+        };
+
+        pub const FluentCommandRegistry = struct {
+            allocator: std.mem.Allocator,
+            commands: []FluentCommand,
+        };
     };
 }
