695b6445217e7face71bf32157d684a01f859341
PierreLgol <plgol.perso@gmail.com>
2024-05-31 11:23:16 +0200

    added methods to FluentCLI structs

--- src/fluent_cli.zig
+++ src/fluent_cli.zig
@@ -8,9 +8,51 @@ pub fn FluentCLI(name: []const u8) type {
         pub const FluentArgument = struct {
             arg_name: []const u8,
             arg_desc: []const u8,
+            arg_regx: []const u8,
             is_required: bool,
             default_value: ?[]const u8,
             validator: ?fn (arg: []const u8) bool,
+
+            pub fn init() FluentArgument {
+                return FluentArgument{
+                    .arg_name = undefined,
+                    .arg_desc = undefined,
+                    .arg_regx = undefined,
+                    .is_required = false,
+                    .default_value = null,
+                    .validator = null,
+                };
+            }
+
+            pub fn setName(self: *FluentArgument, comptime arg_name: []const u8) *FluentArgument {
+                self.arg_name = arg_name;
+                return (self);
+            }
+
+            pub fn setDesc(self: *FluentArgument, comptime arg_desc: []const u8) *FluentArgument {
+                self.arg_desc = arg_desc;
+                return (self);
+            }
+
+            pub fn setRegx(self: *FluentArgument, comptime arg_regx: []const u8) *FluentArgument {
+                self.arg_regx = arg_regx;
+                return (self);
+            }
+
+            pub fn setDefault(self: *FluentArgument, comptime arg_default: []const u8) *FluentArgument {
+                self.default_value = arg_default;
+                return (self);
+            }
+
+            pub fn setRequired(self: *FluentArgument, is_required: bool) *FluentArgument {
+                self.is_required = is_required;
+                return (self);
+            }
+
+            pub fn setValidator(self: *FluentArgument, validator: fn (arg: []const u8) bool) *FluentArgument {
+                self.validator = validator;
+                return (self);
+            }
         };
 
         pub const FluentCommand = struct {
@@ -18,14 +60,75 @@ pub fn FluentCLI(name: []const u8) type {
             flag_shrt: []const u8,
             flag_long: []const u8,
             flag_desc: []const u8,
+            flag_regx: []const u8,
             flag_args: []FluentArgument,
             is_optional: bool,
-            callback: fn (@This(), anytype) bool,
+            callback: ?fn (@This(), anytype) bool,
+
+            pub fn init() FluentCommand {
+                return FluentCommand{
+                    .flag_name = undefined,
+                    .flag_shrt = undefined,
+                    .flag_long = undefined,
+                    .flag_desc = undefined,
+                    .flag_regx = undefined,
+                    .flag_args = undefined,
+                    .is_optional = true,
+                    .callback = null,
+                };
+            }
+
+            pub fn setName(self: *FluentCommand, comptime flag_name: []const u8) *FluentCommand {
+                self.flag_name = flag_name;
+                return (self);
+            }
+
+            pub fn setShort(self: *FluentCommand, comptime flag_shrt: []const u8) *FluentCommand {
+                self.flag_shrt = flag_shrt;
+                return (self);
+            }
+
+            pub fn setLong(self: *FluentCommand, comptime flag_long: []const u8) *FluentCommand {
+                self.flag_long = flag_long;
+                return (self);
+            }
+
+            pub fn setDesc(self: *FluentCommand, comptime flag_desc: []const u8) *FluentCommand {
+                self.flag_desc = flag_desc;
+                return (self);
+            }
+
+            pub fn setRegx(self: *FluentCommand, comptime flag_regx: []const u8) *FluentCommand {
+                self.flag_regx = flag_regx;
+                return (self);
+            }
+
+            pub fn setOptionnal(self: *FluentCommand, optionnal: bool) *FluentCommand {
+                self.is_optional = optionnal;
+                return (self);
+            }
+
+            pub fn setCallback(self: *FluentCommand, callback: fn (cmd: *FluentCommand, receiver: anytype) bool) *FluentCommand {
+                self.callback = callback;
+                return (self);
+            }
+
+            pub fn addArgument(self: *FluentCommand, comptime arg: *FluentArgument) *FluentCommand {
+                self.flag_args = self.flag_args ++ arg; //not correct yet but just for the sake of demo
+                return (self);
+            }
         };
 
         pub const FluentCommandRegistry = struct {
             allocator: std.mem.Allocator,
             commands: []FluentCommand,
+
+            pub fn init(allocator: std.mem.Allocator) FluentCommandRegistry {
+                return FluentCommandRegistry{
+                    .allocator = allocator,
+                    .commands = []FluentCommand,
+                };
+            }
         };
     };
 }
